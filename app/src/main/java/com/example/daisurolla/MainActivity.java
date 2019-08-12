fb5d27b785f587b5c659937bc30bcacb552a1fa8
MistGunOver <pokrywczynski.mat@gmail.com>
2019-08-12 21:34:31 +0200

    initial commit

--- app/src/main/java/com/example/daisurolla/MainActivity.java
+++ app/src/main/java/com/example/daisurolla/MainActivity.java
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     Button plus_circle = findViewById(R.id.plus_circle);
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
